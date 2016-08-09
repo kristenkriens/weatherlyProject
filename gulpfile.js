8a3487ae0082d597cf11390416cdd3cdc1a32b3b
Mark Hill <markjhill84@gmail.com>
2016-08-09 15:16:42 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'user strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
+
+gulp.task('watch', function(){
+	gulp.watch('./dev/styles/**/*.scss', ['styles']);
+	gulp.watch('./dev/scripts/*.js', ['scripts']);
+  	gulp.watch('./public/*.html', reload);
+});
+
+
+gulp.task('styles', function(){
+	return gulp.src('./dev/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/styles/'))
+		.pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: '.'
+  });
+});
+
+gulp.task('scripts', function () {
+  gulp.src('./dev/scripts/main.js')
+  .pipe(gulp.dest('./public/scripts')).pipe(reload({ stream: true }));
+});
